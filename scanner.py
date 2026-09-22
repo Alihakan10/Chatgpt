e1a59535c037417e972270574f33afcde99f721d
Alihakan10 <alihakanyildiz10@gmail.com>
2026-09-22 17:28:35 +0300

    revert diagnostic-only 2H alignment change

--- scanner.py
+++ scanner.py
@@ -1227,16 +1227,10 @@ def get_tv_candles(symbol, candle_mode="session_merged"):
                 continue
             if local_dt.minute != 0:
                 continue
-            if candle_mode == "session_merged_odd":
-                if local_dt.hour % 2 == 1:
-                    start_hour = local_dt.hour
-                else:
-                    start_hour = local_dt.hour - 1
+            if local_dt.hour % 2 == 0:
+                start_hour = local_dt.hour
             else:
-                if local_dt.hour % 2 == 0:
-                    start_hour = local_dt.hour
-                else:
-                    start_hour = local_dt.hour - 1
+                start_hour = local_dt.hour - 1
             key = (local_dt.date(), start_hour)
             grouped.setdefault(key, []).append(bar)
 
