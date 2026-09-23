51f4db12e77349fbc22114f330fcdae9057da69b
Alihakan10 <alihakanyildiz10@gmail.com>
2026-09-23 18:52:14 +0300

    Remove temporary Supertrend diagnostic file

--- diagnose_tv_indicator_metadata.js
+++
@@ -1,7 +0,0 @@
-const ID = "PUB;VfOPXWDHDPhORvJYRTcuHOyeqpOcRR45";
-const url = "https://pine-facade.tradingview.com/pine-facade/translate/" + encodeURIComponent(ID) + "/last";
-const r = await fetch(url, {headers: {"User-Agent":"Mozilla/5.0","Accept":"application/json"}});
-console.log("HTTP", r.status);
-const text = await r.text();
-console.log(text);
-
